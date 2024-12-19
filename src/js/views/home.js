7bd729536d112f70fdcd3bd3459e07a36b391e44
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-12-19 23:55:11 +0000

    extra: background y titulo Star Wars

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,16 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+import titleImage from "../../img/Star_Wars_Logo.png"
+
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div>
+		<div className="content"> 
+			<div className="container w-50 mt-5 pt-5 text-center"> 
+				<img src={titleImage} className="img-fluid" alt="title image"></img>
+				<h1 className="text-white">Bienvenido</h1> 
+				<p className="text-white">Este es un ejemplo de una página con una imagen de fondo fija y centrada.</p> 
+			</div> 
+		</div>
 	</div>
 );
