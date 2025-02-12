7bbd1b7b3f4c96a5a981a37bd7ce167994c72ff8
P. Pablo Cubells Talavera <pablocub@ucm.es>
2025-02-12 16:08:25 +0000

    fuera dropdown

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -11,17 +11,7 @@ export const Navbar = () => {
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" id="profile-tab" data-bs-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Perfil</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" role="button" aria-expanded="false">Dropdown</a>
-                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a className="dropdown-item" id="section1-tab" data-toggle="tab" href="#section1" role="tab" aria-controls="section1" aria-selected="false">Action</a></li>
-                        <li><a className="dropdown-item" id="section2-tab" data-toggle="tab" href="#section2" role="tab" aria-controls="section2" aria-selected="false">Another action</a></li>
-                        <li><a className="dropdown-item" id="section3-tab" data-toggle="tab" href="#section3" role="tab" aria-controls="section3" aria-selected="false">Something else here</a></li>
-                        <li><hr className="dropdown-divider" /></li>
-                        <li><a className="dropdown-item" id="section3-tab" data-toggle="tab" href="#section3" role="tab" aria-controls="section3" aria-selected="false">Separated link</a></li>
-                    </ul>
-                </li>				
+                </li>			
             </ul>
             <Favorites />
         </div>
