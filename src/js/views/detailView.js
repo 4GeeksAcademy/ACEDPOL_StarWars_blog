df6e6de6bdcef138b00ddd88728240e2958133b0
P. Pablo Cubells Talavera <pablocub@ucm.es>
2025-02-15 04:11:09 +0000

    image: desde card a detailView

--- src/js/views/detailView.js
+++ src/js/views/detailView.js
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const DetailView = () => {
     const { store } = useContext(Context);
     const { category, uid } = useParams();
+    const location = useLocation();
+    const { image } = location.state || {};
 
     const item = store[category]?.find(item => item.uid === parseInt(uid));
 
@@ -17,6 +19,7 @@ export const DetailView = () => {
     return (
         <div className="container text-white mt-5">
             <h1>{item.name}</h1>
+            {image && <img src={image} alt={item.name} className="img-fluid mb-3" style={{width: "150px", height: "150px"}} />}
             <ul className="list-group">
                 {Object.entries(item)
                     .filter(([key]) => !excludedKeys.includes(key))
